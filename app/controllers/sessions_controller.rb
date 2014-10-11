5046d866a59564ae88665ca7514c30d7d1432491
William Eustace <weustace@eustacesheen.me.uk>
2014-10-11 21:22:26 +0100

    Created a basic auth system. Needs some refining but satisfactory for testing.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,57 @@
+class SessionsController < ApplicationController
+
+	def login
+		@login_failed = flash[:login_failed]
+		@user = User.new	
+	end
+	
+	def authenticate
+		user = User.find_by(:username => user_auth_params[:username])
+		if !user.nil? && user.try(:authenticate, user_auth_params[:password])
+			#success
+			reset_session
+			#reset the session and give them a session ID
+			session[:uid] = user.id
+			redirect_to root
+			#we want to redirect the user to the device manage page.
+			
+		else
+			#failure
+			#redirect them to the login page & tell them about it
+			flash[:login_failed] = true
+			redirect_to login_path
+		end
+			
+	end
+	def logout
+		reset_session
+		
+	end
+	def new_user
+		#work out how to display errors
+		@errors = flash[:errors]
+		unless @errors.nil?
+			render :text => @errors
+		else
+			@errors = false
+		end
+	end
+	def create
+		new_user = User.new(user_create_params)
+		unless new_user.save
+			flash[:errors] = new_user.errors
+			redirect_to '/sessions/new_user'
+		end
+		redirect_to root
+	end
+	
+	private
+	def user_create_params
+		params.require(:user).permit(:username, :password, :password_confirmation, :email)
+	end
+
+	private 
+	def user_auth_params
+		params.require(:user).permit(:username,:password)#Strong parameters
+	end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get '/login' => 'sessions#login'
+  post 'sessions/authenticate'
+  get '/logout' => 'sessions#logout'
+  get '/sessions/new_user' => 'sessions#new_user'
+  post '/sessions/new_user' => 'sessions#create' 
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
